90e3a345b4d43fb243a9e50eb4590e400e61b44b
Asif Alli <alli.asif@gmail.com>
2020-07-08 20:06:08 -0400

    taken from the java world

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Todos from './components/Todos';
 import Header from './components/layout/Header';
+import AddTodo from './components/AddTodo';
 import './App.css';
 
 class App extends Component {
@@ -45,15 +46,30 @@ class App extends Component {
     this.setState({todos: [...this.state.todos.filter(todo => todo.id !== id)]});
   }
 
+  //Add todo
+  addTodo = (title) => {
+    console.log(title);
+    const newTodo={
+      id:4,
+      title:title,
+      completed:false
+
+    }
+    this.setState({todos:[...this.state.todos,newTodo]})
+  }
+
   render(){
     console.log(this.state.todos);
     return (
-      <div className="App">      
+      <div className="App">  
+        <div className="container">
         <Header/>
+        <AddTodo addTodo={this.addTodo}/>
         <Todos todos={this.state.todos} 
         markComplete={this.markComplete} 
-        delTodo={this.delTodo}
-        />
+        delTodo={this.delTodo}/>
+        </div>    
+
       </div>
     );  
   }
